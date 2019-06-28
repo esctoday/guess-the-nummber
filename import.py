67be4fb6689b61b76b5ddf34050e0c50de973693
esctoday <i.livetoday@hotmail.com>
2019-06-28 23:12:26 +0800

    end

--- import.py
+++ import.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1,100)
+start = input('start nummber:')
+start = int(start)
+end = input('end nummber：')
+end = int(end)
+r = random.randint(start,end)
 count = 0
 while True:
 	count += 1  # count = count + 1
